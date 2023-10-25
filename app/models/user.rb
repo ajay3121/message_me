e723253977fcc49f3cab0eccb4d85235788ee519
gs-ajmali <ajmali@gainsight.com>
2023-10-25 15:38:19 +0530

    add messages to view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: {minimum:3,maximum: 15}
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum:3,maximum: 15}
+  has_many :messages
   has_secure_password
 end
